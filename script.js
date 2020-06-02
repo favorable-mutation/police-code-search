564b34e28c9258ca0053f050b49dc6cd5fdbfcc2
Griffin J Rademacher <griffin@radix.bio>
2020-06-02 00:18:36 -0400

    implements bare minimum functioning app

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$.getJSON("codes.json", function(json) {
-    console.log(json);
+function search(codes, str) {
+  return _.pickBy(codes, (value, code) => code.startsWith(str));
+}
+
+const searchBar = document.getElementById('code-search');
+
+$.getJSON("codes.json", (codes) => {
+  const results = $('#results');
+  _.map(codes, (value, key) => results.append(`<p>${key}: ${value}</p>`));
+
+  searchBar.addEventListener('input', (event) => {
+    results.empty();
+    _.map(search(codes, event.target.value), (value, key) => results.append(`<p>${key}: ${value}</p>`));
+  });
+
 });
