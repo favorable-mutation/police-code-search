2482fb9b2f8af139f057a9c80cddbb3cff3ddf4f
Griffin J Rademacher <griffin@radix.bio>
2020-06-02 12:11:16 -0400

    implements fuzzy search over updated dataset

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-function search(codes, str) {
-  return _.pickBy(codes, (value, code) => code.startsWith(str));
+function search (fuses, codes, str) {
+  return _.mapValues(
+    codes,
+    (vals, code) => _.pick(vals, _.map(fuses[code].search(str), 'item'))
+  )
 }
 
-const searchBar = document.getElementById('code-search');
+function render (codes, results) {
+  _.map(codes, (vals, key) => {
+    results.append(`<h3>${key}</h3>`)
+    _.map(vals, (meaning, code) => {
+      results.append(`<p>${code}: ${meaning}</p>`)
+    })
+  })
+}
 
-$.getJSON("codes.json", (codes) => {
-  const results = $('#results');
-  _.map(codes, (value, key) => results.append(`<p>${key}: ${value}</p>`));
+$.getJSON('codes.json', (codes) => {
+  const searchBar = document.getElementById('code-search')
+  const results = $('#results')
+
+  const fuses = _.mapValues(codes, (vals) => {
+    return new Fuse(_.filter(_.keys(vals), (key) => !key.startsWith('_')), {})
+  })
+
+  render(codes, results)
 
   searchBar.addEventListener('input', (event) => {
-    results.empty();
-    _.map(search(codes, event.target.value), (value, key) => results.append(`<p>${key}: ${value}</p>`));
-  });
-
-});
+    results.empty()
+    render(
+      event.target.value
+        ? search(fuses, codes, event.target.value)
+        : codes,
+      results
+    )
+  })
+})
